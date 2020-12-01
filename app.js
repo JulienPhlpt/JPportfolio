b6ca85adb5d9b796f1f19448bec510be66778659
Julien Philippot <julien.philippot86@gmail.com>
2020-12-01 23:41:09 +0100

    footer design

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let theDate = new Date();
 
 document.querySelector(
   ".date"
-).innerHTML = `&copy; ${theDate.getFullYear()} | All Right Reserved - Made with`;
+).innerHTML = `&copy; ${theDate.getFullYear()} | All Right Reserved - Made with `;
 document.querySelector(
   ".date-mobile"
-).innerHTML = `&copy; ${theDate.getFullYear()} | All Right Reserved - Made with`;
+).innerHTML = `&copy; ${theDate.getFullYear()} | All Right Reserved - Made with `;
