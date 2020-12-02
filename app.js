b65242c3e02227c732949b9261f3a954e4891008
Julien Philippot <julien.philippot86@gmail.com>
2020-12-02 17:46:29 +0100

    Making the navbar responsive

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// GET YEAR
 let theDate = new Date();
 
 document.querySelector(
@@ -6,3 +7,30 @@ document.querySelector(
 document.querySelector(
   ".date-mobile"
 ).innerHTML = `&copy; ${theDate.getFullYear()} | All Right Reserved - Made with `;
+
+// NAVBAR SLIDE
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    // toggle navigation bar
+    nav.classList.toggle("nav-acvtive");
+
+    // Links animation
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
