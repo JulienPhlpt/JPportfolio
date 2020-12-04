cbea05af3c0d5173c377d44f92806642d50077af
Julien Philippot <julien.philippot86@gmail.com>
2020-12-04 16:34:04 +0100

    adding animation

--- app.js
+++ app.js
@@ -33,4 +33,60 @@ const navSlide = () => {
   });
 };
 
+// SCROLL TRIGGER
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline();
+
+tl.from(".content", {
+  y: "-30%",
+  // opacity: 0,
+  duration: 2,
+  ease: Power4.easeOut,
+});
+
+tl.from(
+  ".stagger1",
+  {
+    y: "-50",
+    opacity: 0,
+    duration: 2,
+    stagger: 0.3,
+    ease: Power4.easeOut,
+  },
+  "-=1.2"
+);
+
+tl.from(
+  ".hero-design",
+  {
+    y: "-30%",
+    duration: 2,
+    ease: Power4.easeOut,
+  },
+  "-=3.4"
+);
+
+gsap.from(".transition2", {
+  scrollTrigger: {
+    trigger: ".transition2",
+    start: "top bottom",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.3,
+});
+
+gsap.from(".transition3", {
+  scrollTrigger: {
+    trigger: ".transition3",
+    start: "top center",
+  },
+  y: 50,
+  opacity: 0,
+  duration: 1.2,
+  stagger: 0.6,
+});
+
 navSlide();
